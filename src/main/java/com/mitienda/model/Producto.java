20fed8d8ee5e78c3bef3402e154507e3af409460
Alexis Jacob <ferjacobka@gmail.com>
2026-07-29 22:23:55 -0400

    chore: iniciar base de datos, crear tablas y crearlas en producto tabla = atributos, getters y setters y override de la clase producto

--- src/main/java/com/mitienda/model/Producto.java
+++ src/main/java/com/mitienda/model/Producto.java
@@ -1,4 +1,58 @@
 package com.mitienda.model;
 
 public class Producto {
+
+    private Integer id;
+    private String nombre;
+    private Double precio;
+    private int stock;
+    private String categoria;
+
+    // constructor completo
+    public Producto(Integer id, String nombre, Double precio, int stock, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    // constructor en caso de que sea libro nuevo s
+    public Producto(String nombre, Double precio, int stock, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    // override
+    @Override
+    public String toString() {
+        return String.format("[%d] %s | Categoria: %s | $%.2f | Stock: %d", id, nombre, categoria, precio, stock);
+    }
 }
